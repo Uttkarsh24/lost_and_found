6233b48a7f3b3c6c3d050cf90580c09065bfc4a3
Uttkarsh24 <uttkarshsingh1973@gmail.com>
2025-05-09 10:22:50 +0530

    added major files

---
+++ app/src/main/java/com/shruti/lofo/Login.java
@@ -0,0 +1,15 @@
+package com.shruti.lofo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+// Removed Login activity code
---
+++ app/src/main/java/com/shruti/lofo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.shruti.lofo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bind_nav); // Set the main page layout directly
+    }
+}
